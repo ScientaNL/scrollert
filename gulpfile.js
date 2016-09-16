91bc03db345348c16a8c84cad7b91c78e8b4c1de
Harmen <harmen@Harmens-MacBook-Pro.local>
2016-09-16 17:21:11 +0200

    Working on automated build of docs directory and github.io website based on README.md

--- gulpfile.js
+++ gulpfile.js
@@ -8,45 +8,76 @@ var fs = require('fs'),
 	less = require('gulp-less'),
 	autoprefixer = require('gulp-autoprefixer'),
 	cssmin = require('gulp-cssmin'),
-	wrapper = require('gulp-wrapper');
-
+	wrapper = require('gulp-wrapper'),
+    del = require('del'),
+    debug = require('gulp-debug'),
+    gulpif = require('gulp-if'),
+    staticSiteGenerator = require('gulp-static-site-generator');
 
 gulp.task('typescript', function() {
 
-	var tsProject = ts.createProject(__dirname + '/tsconfig.json', {sortOutput: true}),
-		jsWrapper = fs.readFileSync(__dirname + "/build/typescript/jquery-umd.js", 'utf8').split("/** {CODE} */");
+	var tsProject = ts.createProject('tsconfig.json', {sortOutput: true}),
+		jsWrapper = fs.readFileSync("build/typescript/jquery-umd.js", 'utf8').split("/** {CODE} */");
 
 	if(jsWrapper.length !== 2) throw new TypeError("Invalid wrapper template specified");
 
-	return tsProject.src(__dirname + "/src/index.ts")
+	return tsProject.src("src/index.ts")
 		.pipe(sourcemaps.init())
 		.pipe(ts(tsProject))
 		.pipe(concat("scrollert.js"))
 		.pipe(wrapper({header: jsWrapper[0], footer: jsWrapper[1]}))
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(__dirname + "/dist"))
+		.pipe(gulp.dest("dist"))
 		.pipe(uglify())
 		.pipe(rename({ extname: '.min.js' }))
-		.pipe(gulp.dest(__dirname + "/dist"));
+		.pipe(gulp.dest("dist"));
 });
 
 gulp.task('less', function() {
 
-	return gulp.src(__dirname + "/src/scrollert.less")
+	return gulp.src("src/scrollert.less")
 		.pipe(sourcemaps.init())
 		.pipe(less())
 		.pipe(autoprefixer())
 		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(__dirname + "/dist"))
+		.pipe(gulp.dest("dist"))
 		.pipe(cssmin())
 		.pipe(rename({ extname: '.min.css' }))
-		.pipe(gulp.dest(__dirname + "/dist"));
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task('generate-docs', ['clean-docs'], function() {
+    return gulp.src(['build/docs/site/**/*', '*.md', "dist/*.min.*"])
+        .pipe(debug())
+        .pipe(rename(function(path) {
+
+            //Tuple Github's landing README.md as into the index of the docs-website
+            if(path.basename === "README" && path.extname === ".md")
+            {
+                path.basename = "index";
+            }
+        }))
+        .pipe(staticSiteGenerator({
+            defaultLayout: 'base.jade',
+            layoutPath: 'build/docs/layouts',
+            jadeOptions: {
+                pretty: true
+            }
+        }))
+        .pipe(gulpif(/\.less$/, less()))
+        .pipe(gulp.dest('docs'));
+});
+
+gulp.task('clean-docs', function(done) {
+    del('./docs').then(function() {
+        done();
+    });
 });
 
 gulp.task('watch', function() {
-	gulp.watch(__dirname + "/src/*.ts", ['typescript']);
-	gulp.watch(__dirname + "/src/*.less", ['less']);
+	gulp.watch("src/*.ts", ['typescript']);
+	gulp.watch("src/*.less", ['less']);
 });
 
-gulp.task('build', ['typescript', 'less']);
+gulp.task('build', ['typescript', 'less', 'generate-docs']);
 gulp.task('default', ['watch', 'build']);
